5415e42589b3a8137114dbc02d3dc5621caeb950
Daria Kuliashova <daria.kuliashova@scootapi.com>
2024-11-05 09:39:19 +0100

    fix new-promotion

--- src/app/(admin)/companies/[id]/not-found.tsx
+++ src/app/(admin)/companies/[id]/not-found.tsx
@@ -2,9 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-export interface NotFoundProps {}
-
-function NotFound({}: NotFoundProps) {
+function NotFound() {
   return (
     <div>
       <p>Could not found company</p>
